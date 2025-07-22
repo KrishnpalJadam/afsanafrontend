40ba51d7c5851af2b78b9daf2d407e59bd6c260f
Arun-badode <arunbadode08@gmail.com>
2025-07-22 18:25:52 +0530

    first commit

--- src/MyProfile/MyProfile.jsx
+++ src/MyProfile/MyProfile.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Card, ListGroup, Container, Row, Col, Form, Button, Modal } from "react-bootstrap";
+import {
+  Card,
+  ListGroup,
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Modal,
+} from "react-bootstrap";
 import api from "../interceptors/axiosInterceptor";
-
 
 const MyProfile = () => {
   const loginDetail = JSON.parse(localStorage.getItem("login_detail"));
@@ -20,7 +28,7 @@ const MyProfile = () => {
     admission_no: "",
     id_no: "",
     category: "",
-    university_id: ""
+    university_id: "",
   });
 
   const fetchUserData = async () => {
@@ -37,12 +45,14 @@ const MyProfile = () => {
           phone: user.phone || "",
           address: user.address || "",
           gender: user.gender || "",
-          date_of_birth: user.date_of_birth ? user.date_of_birth.split("T")[0] : "",
+          date_of_birth: user.date_of_birth
+            ? user.date_of_birth.split("T")[0]
+            : "",
           father_name: user.father_name || "",
           admission_no: user.admission_no || "",
           id_no: user.id_no || "",
           category: user.category || "",
-          university_id: user.university_id || ""
+          university_id: user.university_id || "",
         });
       }
     } catch (error) {
@@ -72,7 +82,10 @@ const MyProfile = () => {
     filteredFormData["user_id"] = loginDetail.id;
 
     try {
-      const res = await api.put(`auth/updateUser/${loginDetail.id}`, filteredFormData);
+      const res = await api.put(
+        `auth/updateUser/${loginDetail.id}`,
+        filteredFormData
+      );
       if (res.status === 200) {
         alert("Profile updated successfully!");
         setShowModal(false);
@@ -107,10 +120,13 @@ const MyProfile = () => {
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
-        <Col md={6}>
-          <Card className="shadow-sm rounded-4 border-0 overflow-hidden">
-            <Card.Header as="h4" className="text-center bg-primary text-white py-3 fw-bold">
-              My Profile
+        <Col md={12}>
+          <Card className="container-flud shadow-sm rounded-4 border-0 overflow-hidden">
+            <Card.Header
+              as="h4"
+              className="text-center bg-primary text-white py-3 fw-bold"
+            >
+              GENERAL INFORMATION
             </Card.Header>
 
             <ListGroup variant="flush">
@@ -118,21 +134,320 @@ const MyProfile = () => {
               {renderItem("Email", userData?.email)}
               {renderItem("Phone", userData?.phone)}
               {renderItem("Gender", userData?.gender)}
-              {renderItem("Date of Birth", userData?.date_of_birth && new Date(userData.date_of_birth).toLocaleDateString())}
+              {renderItem(
+                "Date of Birth",
+                userData?.date_of_birth &&
+                  new Date(userData.date_of_birth).toLocaleDateString()
+              )}
               {renderItem("Address", userData?.address)}
               {renderItem("Father's Name", userData?.father_name)}
-            
-              {renderItem("ID No.", userData?.id_no)}
-            </ListGroup>
 
-            <Card.Footer className="text-center bg-light">
+              {renderItem("ID No.", userData?.id_no)}
+              {renderItem("TIN No.", userData?.phone)}
+              {renderItem("Present Address ", userData?.phone)}
+              {renderItem("Permanent Address ", userData?.phone)}
+              {renderItem("Marital Status ", userData?.phone)}
+              {renderItem("Spouse’s Occupation (If married) ", userData?.phone)}
+              {renderItem(
+                "Spouse’s Monthly Income (If married)",
+                userData?.phone
+              )}
+              {renderItem("Number of Children (If married)", userData?.phone)}
+              {renderItem("Who Will Be Your Sponsor? ", userData?.phone)}
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col md={12}>
+          <div className=" shadow-sm rounded-4 border overflow-hidden">
+            <div className="bg-primary text-white py-3 fw-bold text-center h4">
+              ACADEMIC INFORMATION
+            </div>
+
+            <table className="table table-bordered mb-0 text-center">
+              <thead className="table-light">
+                <tr>
+                  <th>Institute Name & Address</th>
+                  <th>Degree</th>
+                  <th>Group/Department</th>
+                  <th>Result</th>
+                  <th>Duration (Start–End DD/MM/YYYY)</th>
+                  <th>Fail/Retake/Withdraw/Transfer</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="text-center bg-light py-3">
               <Button variant="primary" onClick={() => setShowModal(true)}>
-                Update Profile
+                Add more
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-center mt-5">
+        <Col md={12}>
+          <Card className="container-flud shadow-sm rounded-4 border-0 overflow-hidden">
+            <Card.Header
+              as="h4"
+              className="text-center bg-primary text-white py-3 fw-bold"
+            >
+              ENGLISH PROFICIENCY TEST (EPT) SCORE
+            </Card.Header>
+
+            <ListGroup variant="flush">
+              {renderItem("EPT Name ", userData?.full_name)}
+              {renderItem("Expiry Date ", userData?.full_name)}
+              {renderItem("Overall Score", userData?.full_name)}
+              {renderItem("Result", userData?.full_name)}
+              {renderItem("Listening", userData?.full_name)}
+              {renderItem("Reading ", userData?.full_name)}
+              {renderItem("Speaking  ", userData?.full_name)}
+              {renderItem("Writing ", userData?.full_name)}
+            </ListGroup>
+            <Card.Footer className="text-center bg-light mt-5">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
               </Button>
             </Card.Footer>
           </Card>
         </Col>
       </Row>
+
+      <Row className="mt-5">
+        <Col md={12}>
+          <div className=" shadow-sm rounded-4 border overflow-hidden">
+            <div className="bg-primary text-white py-3 fw-bold text-center h4">
+              APPLICANT’S JOB/PROFESSIONAL DETAILS
+            </div>
+
+            <table className="table table-bordered mb-0 text-center">
+              <thead className="table-light">
+                <tr>
+                  <th>Company & Designation </th>
+                  <th>Monthly Income </th>
+                  <th>Payment Method </th>
+                  <th>Bank Name & Account Type</th>
+                  <th>Employment Duration </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="text-center bg-light py-3">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-center mt-5">
+        <Col md={12}>
+          <Card className="container-flud shadow-sm rounded-4 border-0 overflow-hidden">
+            <Card.Header
+              as="h4"
+              className="text-center bg-primary text-white py-3 fw-bold"
+            >
+              TRAVEL & PASSPORT DETAILS
+            </Card.Header>
+
+            <ListGroup variant="flush">
+              {renderItem(
+                "Previously Refused Countries (Name, Year, Reason) ",
+                userData?.full_name
+              )}
+              {renderItem(
+                "Travel History (Country Name, Year)",
+                userData?.full_name
+              )}
+              {renderItem("Overall Score", userData?.full_name)}
+              {renderItem(
+                "Current Passport  No. & Expiry Date",
+                userData?.full_name
+              )}
+              {renderItem("Passport 2 No. & Expiry Date ", userData?.full_name)}
+              {renderItem("Passport 3 No. & Expiry Date ", userData?.full_name)}
+            </ListGroup>
+            <Card.Footer className="text-center bg-light mt-5">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col md={12}>
+          <div className=" shadow-sm rounded-4 border overflow-hidden">
+            <div className="bg-primary text-white py-3 fw-bold text-center h4">
+              BUSINESS DETAILS (IF ANY)
+            </div>
+            <div className="table-responsive">
+              <table className="table- table-bordered mb-0 text-center">
+                <thead className="table-light">
+                  <tr>
+                    <th>Business Name & License No. </th>
+                    <th>Monthly Income & Current Balance </th>
+                    <th>Personal Savings (Bank, Type, Branch, Amount) </th>
+                    <th>Business Income Bank Name & Type</th>
+                    <th>Tax Returns (3 years) & TIN Certificate </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{userData?.full_name}</td>
+                    <td>{userData?.full_name}</td>
+                    <td>{userData?.full_name}</td>
+                    <td>{userData?.full_name}</td>
+                    <td>{userData?.full_name}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div className="text-center bg-light py-3">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-center mt-5">
+        <Col md={12}>
+          <Card className="container-flud shadow-sm rounded-4 border-0 overflow-hidden">
+            <Card.Header
+              as="h4"
+              className="text-center bg-primary text-white py-3 fw-bold"
+            >
+              SPONSOR’S INFORMATION
+            </Card.Header>
+
+            <ListGroup variant="flush">
+              {renderItem("Name & Email", userData?.full_name)}
+              {renderItem("Relationship", userData?.full_name)}
+              {renderItem("Occupation ", userData?.full_name)}
+              {renderItem("Job Position, Company", userData?.full_name)}
+              {renderItem("Employment Duration", userData?.full_name)}
+              {renderItem(
+                "Status (Employed/Unemployed/Business)",
+                userData?.full_name
+              )}
+              {renderItem("Business TIN/BIN (if any)", userData?.full_name)}
+              {renderItem(
+                "Tax Documents Available (Yes/No)",
+                userData?.full_name
+              )}
+              {renderItem("Present Address ", userData?.full_name)}
+              {renderItem("Phone", userData?.full_name)}
+            </ListGroup>
+            <Card.Footer className="text-center bg-light mt-5">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+
+     
+
+      <Row className="mt-5">
+        <Col md={12}>
+          <div className="shadow-sm rounded-4 border overflow-hidden">
+            <div className="bg-primary text-white py-3 fw-bold text-center h4">
+              SPONSOR’S BUSINESS DETAILS
+            </div>
+
+            <table className="table table-bordered mb-0 text-center">
+              <thead className="table-light">
+                <tr>
+                  <th>Business Name</th>
+                  <th>Type</th>
+                  <th>Income (Monthly & Yearly)</th>
+                  <th>License No.</th>
+                  <th>Income Received via (Cash/Bank)</th>
+                  <th>Bank Details (if bank)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                  <td>{userData?.full_name}</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="text-center bg-light py-3">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+        <Row className="justify-content-center mt-5">
+        <Col md={12}>
+          <Card className="container-flud shadow-sm rounded-4 border-0 overflow-hidden">
+            <Card.Header
+              as="h4"
+              className="text-center bg-primary text-white py-3 fw-bold"
+            >
+              INFORMATION FOR COVER LETTER
+            </Card.Header>
+
+            <ListGroup variant="flush">
+              {renderItem("Visa Refusal Explanation", userData?.full_name)}
+              {renderItem("Any Name/Age Mismatches (Self or Parents)", userData?.full_name)}
+              {renderItem("Study Gap Explanation", userData?.full_name)}
+              {renderItem("Job Position, Company", userData?.full_name)}
+              {renderItem("Deportation Details (if any) ", userData?.full_name)}
+
+            </ListGroup>
+            <Card.Footer className="text-center bg-light mt-5">
+              <Button variant="primary" onClick={() => setShowModal(true)}>
+                Add more
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+
+
+      <Card.Footer className="text-center bg-light mt-5">
+        <Button variant="primary" onClick={() => setShowModal(true)}>
+          Update Profile
+        </Button>
+      </Card.Footer>
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
@@ -214,8 +529,6 @@ const MyProfile = () => {
                     onChange={handleChange}
                   />
                 </Form.Group>
-
-               
 
                 <Form.Group className="mb-3">
                   <Form.Label>ID No.</Form.Label>
